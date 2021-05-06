65f148a4ae0169d7e6d9abe9a25e0b4bd9180177
Christoph Lehmann <christoph.lehmann@cgi.com>
2021-05-06 21:39:20 +0200

    initial commit

---
+++ src/dev/hive/MazeSolver.java
@@ -0,0 +1,18 @@
+package dev.hive;
+
+public class MazeSolver {
+    public static void main(String[] args) {
+        Maze maze = new Maze();
+
+        System.out.println(maze);
+
+        /* Aufgaben
+         * ========
+         *
+         * 1. Schreibt einen Algorithmus, er den Weg aus dem Labyrinth findet.
+         *    Der Ausweg soll (a) ausgegeben werden (println) und (b) als Liste von Koordinaten (x, y) zurückgegeben werden.
+         * 2. Schreibt sinnvolle Tests für euren Algorithmus
+         * 3. Schreibt einen Algorithmus, der den kürzesten Weg aus dem Labyrinth findet.
+         */
+    }
+}
